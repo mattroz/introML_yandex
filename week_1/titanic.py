459f5fcd0c2b4381b3811fdac4bf85823580f687
mattroz <matveyrozanov@gmail.com>
2017-07-22 23:02:18 +0300

    Implemented the third task

--- week_1/titanic.py
+++ week_1/titanic.py
@@ -12,10 +12,10 @@ data = pd.read_csv('titanic.csv', index_col='PassengerId')
 
 
 # Get number of male and female passengers
-def count_sex(dataframe, filename):
+def count_sex(dataframe, _filename):
     sex_dict = {'female': 0,
                 'male': 0}
-    file = open(filename, 'w')
+    file = open(_filename, 'w')
     for sex in sex_dict:
         sex = dataframe['Sex'].value_counts()[sex]
         file.write(str(sex) + ' ')
@@ -23,14 +23,27 @@ def count_sex(dataframe, filename):
 
 
 # Get survived in percents
-def survived_in_percents(dataframe, filename):
+def survived_in_percents(dataframe, _filename):
     passengers_number = dataframe.index[-1]
     if_survived = 1
     survived = \
         round(dataframe['Survived'].value_counts()[if_survived] * 100 / passengers_number, 2)
     print(survived)
-    file = open(filename, 'w')
+    file = open(_filename, 'w')
     file.write(str(survived) + ' ')
     file.close()
 
-survived_in_percents(data, filename[1])
+
+# Get percent of first class passengers relative to the general number of passengers.
+def get_first_class_percentage(dataframe, _filename):
+    passengers_number = dataframe.index[-1]
+    if_first_class = 1
+    firstclass = \
+        round(dataframe['Pclass'].value_counts()[if_first_class] * 100 / passengers_number, 2)
+    print(firstclass)
+    file = open(_filename, 'w')
+    file.write(str(firstclass) + ' ')
+    file.close()
+
+get_first_class_percentage(data, filename[2])
+#survived_in_percents(data, filename[1])
